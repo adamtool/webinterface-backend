521a014979c612642611063191013a4028ac44ac
Manuel Gieseking <mgieseking@users.noreply.github.com>
2019-07-02 18:40:36 +0200

    MG: finished some first optimizations and added scripts for comparing those.

--- src/uniolunisaar/adam/main/AdamModelcheckerATVA2019.java
+++ src/uniolunisaar/adam/main/AdamModelcheckerATVA2019.java
@@ -15,7 +15,6 @@ import uniolunisaar.adam.logic.externaltools.modelchecking.Abc;
 import uniolunisaar.adam.logic.externaltools.modelchecking.Abc.VerificationAlgo;
 import uniolunisaar.adam.logic.modelchecking.circuits.ModelCheckerFlowLTL;
 import uniolunisaar.adam.logic.parser.logics.flowltl.FlowLTLParser;
-import uniolunisaar.adam.logic.transformers.pn2aiger.AigerRenderer;
 import uniolunisaar.adam.tools.Tools;
 import uniolunisaar.adam.util.PNWTTools;
 import uniolunisaar.adam.util.logics.transformers.logics.ModelCheckingOutputData;
@@ -24,14 +23,14 @@ import uniolunisaar.adam.util.logics.transformers.logics.ModelCheckingOutputData
  *
  * @author Manuel Gieseking
  */
-public class AdamModelchecker {
+public class AdamModelcheckerATVA2019 {
 
     /**
      *
      * @param args 0 -> Input path to APT 1 -> Output path for data 2 ->
      * Verifier 3 -> ABC parameters 4 -> Formula (if "", then it's expected to
-     * be annotated within the APT input file) 5 -> optimized rendering 6 -> if
-     * != "" sizes would be written to the given path
+     * be annotated within the APT input file) 5 -> if != "" sizes would be
+     * written to the given path
      *
      * @throws ParseException
      * @throws IOException
@@ -51,15 +50,15 @@ public class AdamModelchecker {
         String output = args[1];
 
         ModelcheckingStatistics stats = null;
-        if (!args[6].isEmpty()) {
-            stats = new ModelcheckingStatistics(args[6]);
+        if (!args[5].isEmpty()) {
+            stats = new ModelcheckingStatistics(args[5]);
         } else {
             stats = new ModelcheckingStatistics();
         }
         stats.setPrintSysCircuitSizes(false);
         // add nb switches to file for the SDN paper        
-        if (!args[6].isEmpty()) {
-            try (BufferedWriter wr = new BufferedWriter(new FileWriter(args[6] + "_sw"))) {
+        if (!args[5].isEmpty()) {
+            try (BufferedWriter wr = new BufferedWriter(new FileWriter(args[5] + "_sw"))) {
                 wr.append("nb_switches: ").append((CharSequence) pnwt.getExtension("nb_switches"));
             }
         }
@@ -80,15 +79,7 @@ public class AdamModelchecker {
 
         String abcParameter = args[3];
 
-        String optimizations = args[5];
-        AigerRenderer.Optimizations optis = AigerRenderer.Optimizations.NONE;
-        if (optimizations.equals("GATES")) {
-            optis = AigerRenderer.Optimizations.NB_GATES;
-        } else if (optimizations.equals("GATES_AND_INDICES")) {
-            optis = AigerRenderer.Optimizations.NB_GATES_AND_INDICES;
-        }
-
-        ModelCheckerFlowLTL mc = new ModelCheckerFlowLTL(optis);
+        ModelCheckerFlowLTL mc = new ModelCheckerFlowLTL();
         if (algo != null) {
             mc.setVerificationAlgo(algo);
         }
@@ -96,7 +87,7 @@ public class AdamModelchecker {
 
         ModelCheckingOutputData data = new ModelCheckingOutputData(output, false, false, false);
         mc.check(pnwt, f, data, stats);
-        if (!args[6].isEmpty()) {
+        if (!args[5].isEmpty()) {
             stats.addResultToFile();
         }
 

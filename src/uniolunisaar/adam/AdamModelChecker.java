819b3ee26a822c72a9b677a728dd68c817b39332
mgieseking <gieseking@informatik.uni-oldenburg.de>
2020-10-03 17:35:47 +0200

    Added the precondition checker for the synthesis approach

--- src/uniolunisaar/adam/AdamModelChecker.java
+++ src/uniolunisaar/adam/AdamModelChecker.java
@@ -38,7 +38,7 @@ import uniolunisaar.adam.logic.transformers.modelchecking.pnwt2pn.withoutinittfl
 import uniolunisaar.adam.logic.transformers.modelchecking.pnwt2pn.withoutinittflplaces.PnwtAndNbFlowFormulas2PNSeqInhibitorNoInit;
 import uniolunisaar.adam.logic.transformers.modelchecking.pnwt2pn.withoutinittflplaces.PnwtAndNbFlowFormulas2PNSequentialNoInit;
 import uniolunisaar.adam.util.MCTools;
-import uniolunisaar.adam.util.PreconditionChecker;
+import uniolunisaar.adam.util.PnwtPreconditionChecker;
 import uniolunisaar.adam.util.logics.LogicsTools;
 
 /**
@@ -47,8 +47,8 @@ import uniolunisaar.adam.util.logics.LogicsTools;
  */
 public class AdamModelChecker {
 
-    public static PreconditionChecker createPreconditionChecker(PetriNetWithTransits pnwt) {
-        return new PreconditionChecker(pnwt);
+    public static PnwtPreconditionChecker createPreconditionChecker(PetriNetWithTransits pnwt) {
+        return new PnwtPreconditionChecker(pnwt);
     }
 
     // %%%%%%%%%%%%%%%%%%%%%%%%%%%% GENERATORS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

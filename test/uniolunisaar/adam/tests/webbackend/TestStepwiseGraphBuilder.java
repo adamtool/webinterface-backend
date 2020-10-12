53dce8c1541a166159e60d3be4080418408f4a61
mgieseking <gieseking@informatik.uni-oldenburg.de>
2020-10-13 01:53:05 +0200

    Delete the examples dependecies of the tests.

--- test/uniolunisaar/adam/tests/webbackend/TestStepwiseGraphBuilder.java
+++ test/uniolunisaar/adam/tests/webbackend/TestStepwiseGraphBuilder.java
@@ -36,14 +36,107 @@ public class TestStepwiseGraphBuilder {
 
     @Test
     public void testStepwise() throws IOException, ParseException, CouldNotFindSuitableConditionException, SolvingException, CalculationInterruptedException {
-        final String path = inputDir + "burglar" + File.separator;
-        final String name = "burglar";
-        PetriGameWithTransits game = PGTools.getPetriGame(path + name + ".apt", true, false);
-        
+//        final String path = inputDir + "burglar" + File.separator;
+//        final String name = "burglar";
+
+        String apt = ".name \"burglar.net\"\n"
+                + ".description \"alarm system\"\n"
+                + ".type LPN\n"
+                + ".options\n"
+                + "condition=\"SAFETY\"\n"
+                + "\n"
+                + ".places\n"
+                + "a[yCoord=330.0, xCoord=750.0]\n"
+                + "aa[yCoord=990.0, xCoord=390.0]\n"
+                + "ab[yCoord=990.0, xCoord=630.0]\n"
+                + "b[yCoord=330.0, xCoord=1230.0]\n"
+                + "ba[yCoord=990.0, xCoord=1350.0]\n"
+                + "bb[yCoord=990.0, xCoord=1590.0]\n"
+                + "ea[yCoord=330.0, xCoord=390.0, env=\"true\"]\n"
+                + "eb[yCoord=330.0, xCoord=1590.0, env=\"true\"]\n"
+                + "env[yCoord=150.0, xCoord=990.0, env=\"true\"]\n"
+                + "pa[yCoord=690.0, xCoord=510.0]\n"
+                + "pb[yCoord=690.0, xCoord=1470.0]\n"
+                + "qbadA[yCoord=810.0, bad=\"true\", xCoord=2250.0]\n"
+                + "qbadB[yCoord=210.0, bad=\"true\", xCoord=2250.0]\n"
+                + "s1[yCoord=150.0, xCoord=510.0, env=\"true\"]\n"
+                + "s2[yCoord=150.0, xCoord=1470.0, env=\"true\"]\n"
+                + "s3[yCoord=450.0, xCoord=510.0]\n"
+                + "s4[yCoord=450.0, xCoord=1470.0]\n"
+                + "\n"
+                + ".transitions\n"
+                + "t1[label=\"t1\", yCoord=570.0, xCoord=510.0]\n"
+                + "t10[label=\"t10\", yCoord=810.0, xCoord=1830.0]\n"
+                + "t11[label=\"t11\", yCoord=690.0, xCoord=1830.0]\n"
+                + "t12[label=\"t12\", yCoord=210.0, xCoord=1830.0]\n"
+                + "t13[label=\"t13\", yCoord=450.0, xCoord=2010.0]\n"
+                + "t14[label=\"t14\", yCoord=690.0, xCoord=2010.0]\n"
+                + "t15[label=\"t15\", yCoord=330.0, xCoord=2010.0]\n"
+                + "t16[label=\"t16\", yCoord=570.0, xCoord=2010.0]\n"
+                + "t2[label=\"t2\", yCoord=570.0, xCoord=630.0]\n"
+                + "t3[label=\"t3\", yCoord=570.0, xCoord=1350.0]\n"
+                + "t4[label=\"t4\", yCoord=570.0, xCoord=1470.0]\n"
+                + "t5[label=\"t5\", yCoord=810.0, xCoord=630.0]\n"
+                + "t6[label=\"t6\", yCoord=810.0, xCoord=390.0]\n"
+                + "t7[label=\"t7\", yCoord=810.0, xCoord=1590.0]\n"
+                + "t8[label=\"t8\", yCoord=810.0, xCoord=1350.0]\n"
+                + "t9[label=\"t9\", yCoord=330.0, xCoord=1830.0]\n"
+                + "ta[label=\"ta\", yCoord=330.0, xCoord=510.0]\n"
+                + "taa[label=\"taa\", yCoord=570.0, xCoord=930.0]\n"
+                + "tb[label=\"tb\", yCoord=330.0, xCoord=1470.0]\n"
+                + "tbb[label=\"tbb\", yCoord=570.0, xCoord=1050.0]\n"
+                + "tt1[label=\"tt1\", yCoord=-210.0, xCoord=2010.0]\n"
+                + "tt2[label=\"tt2\", yCoord=30.0, xCoord=2010.0]\n"
+                + "tt3[label=\"tt3\", yCoord=-90.0, xCoord=2010.0]\n"
+                + "tt4[label=\"tt4\", yCoord=150.0, xCoord=2010.0]\n"
+                + "tt5[label=\"tt5\", yCoord=1110.0, xCoord=2010.0]\n"
+                + "tt6[label=\"tt6\", yCoord=870.0, xCoord=2010.0]\n"
+                + "tt7[label=\"tt7\", yCoord=1230.0, xCoord=2010.0]\n"
+                + "tt8[label=\"tt8\", yCoord=990.0, xCoord=2010.0]\n"
+                + "tx[label=\"tx\", yCoord=150.0, xCoord=750.0]\n"
+                + "ty[label=\"ty\", yCoord=150.0, xCoord=1230.0]\n"
+                + "\n"
+                + ".flows\n"
+                + "t1: {1*s3} -> {1*pa}\n"
+                + "t10: {1*ea, 1*bb} -> {1*ea, 1*qbadB}\n"
+                + "t11: {1*ea, 1*ab} -> {1*qbadA, 1*ea}\n"
+                + "t12: {1*ba, 1*eb} -> {1*qbadB, 1*eb}\n"
+                + "t13: {1*env, 1*aa} -> {1*env, 1*qbadA}\n"
+                + "t14: {1*env, 1*ab} -> {1*qbadA, 1*env}\n"
+                + "t15: {1*ba, 1*env} -> {1*qbadB, 1*env}\n"
+                + "t16: {1*bb, 1*env} -> {1*env, 1*qbadB}\n"
+                + "t2: {1*a} -> {1*pa}\n"
+                + "t3: {1*b} -> {1*pb}\n"
+                + "t4: {1*s4} -> {1*pb}\n"
+                + "t5: {1*pa} -> {1*ab}\n"
+                + "t6: {1*pa} -> {1*aa}\n"
+                + "t7: {1*pb} -> {1*bb}\n"
+                + "t8: {1*pb} -> {1*ba}\n"
+                + "t9: {1*aa, 1*eb} -> {1*qbadA, 1*eb}\n"
+                + "ta: {1*a, 1*s1} -> {1*ea, 1*s3}\n"
+                + "taa: {1*s3, 1*b} -> {1*pb, 1*pa}\n"
+                + "tb: {1*b, 1*s2} -> {1*eb, 1*s4}\n"
+                + "tbb: {1*a, 1*s4} -> {1*pb, 1*pa}\n"
+                + "tt1: {1*s1, 1*aa} -> {1*qbadA, 1*s1}\n"
+                + "tt2: {1*s1, 1*ab} -> {1*s1, 1*qbadA}\n"
+                + "tt3: {1*s1, 1*ba} -> {1*qbadB, 1*s1}\n"
+                + "tt4: {1*s1, 1*bb} -> {1*s1, 1*qbadB}\n"
+                + "tt5: {1*aa, 1*s2} -> {1*qbadA, 1*s2}\n"
+                + "tt6: {1*s2, 1*ab} -> {1*s2, 1*qbadA}\n"
+                + "tt7: {1*s2, 1*ba} -> {1*s2, 1*qbadB}\n"
+                + "tt8: {1*s2, 1*bb} -> {1*s2, 1*qbadB}\n"
+                + "tx: {1*env} -> {1*s1}\n"
+                + "ty: {1*env} -> {1*s2}\n"
+                + "\n"
+                + ".initial_marking {1*a, 1*b, 1*env}";
+
+//        PetriGameWithTransits game = PGTools.getPetriGame(path + name + ".apt", true, false);
+        PetriGameWithTransits game = PGTools.getPetriGameFromAPTString(apt, true, false);
+
         BDDGraph bddGraph = new BDDGraph("test");
         GGBuilderStepwise builder = new GGBuilderStepwise(game, bddGraph);
         builder.addSuccessors(bddGraph.getInitial(), game, bddGraph);
-        
+
 //        for (BDDState state : bddGraph.getStates()) {
 //            System.out.println(state.getContent());
 //            System.out.println("neext");
@@ -52,7 +145,6 @@ public class TestStepwiseGraphBuilder {
 //                System.out.println(state1.getContent());
 //            }
 //        }
-
 //        BDDSolverOptions opts = new BDDSolverOptions(true, true);
 //        DistrSysBDDSolver<? extends Condition> solv = DistrSysBDDSolverFactory.getInstance().getSolver(path + name + ".apt", opts);
 //        solv.initialize();
--- test/uniolunisaar/adam/tests/webbackend/TestingMCFormulaCreation.java
+++ test/uniolunisaar/adam/tests/webbackend/TestingMCFormulaCreation.java
@@ -37,12 +37,12 @@ public class TestingMCFormulaCreation {
     }
 
     /**
-     * 
+     *
      * @param net
      * @param formula
      * @return
      * @throws ParseException
-     * @throws NotConvertableException 
+     * @throws NotConvertableException
      */
     public ILTLFormula createLTLFormula(PetriNetWithTransits net, String formula) throws ParseException, NotConvertableException {
         RunLTLFormula runFormula = AdamModelChecker.parseFlowLTLFormula(net, formula);
@@ -57,10 +57,66 @@ public class TestingMCFormulaCreation {
                 net, modelCheckingNet, runFormula, settings);
     }
 
-    @Test(enabled=true)
+    @Test(enabled = true)
     public void testMCFormulaCreation() throws ParseException, IOException, NotConvertableException {
-        String path = inputDir + "ATVA19_motivatingExample.apt";
-        PetriNetWithTransits net = PNWTTools.getPetriNetWithTransitsFromFile(path, false);
+//        String path = inputDir + "ATVA19_motivatingExample.apt";
+//        PetriNetWithTransits net = PNWTTools.getPetriNetWithTransitsFromFile(path, false);
+        String content = ".name \"MotivatingExample\"\n"
+                + ".description \"The motivating example of the ATVA19 conference.\"\n"
+                + ".type LPN\n"
+                + ".options\n"
+                + "condition=\"LTL\"\n"
+                + "\n"
+                + ".places\n"
+                + "A[yCoord=930.0, xCoord=690.0]\n"
+                + "C[yCoord=750.0, xCoord=1290.0]\n"
+                + "L[yCoord=930.0, xCoord=1770.0]\n"
+                + "P[yCoord=510.0, xCoord=1770.0]\n"
+                + "S[yCoord=510.0, xCoord=690.0]\n"
+                + "p10[yCoord=990.0, xCoord=990.0]\n"
+                + "p11[yCoord=450.0, xCoord=990.0]\n"
+                + "p12[yCoord=736.84, xCoord=544.2]\n"
+                + "p13[yCoord=1230.0, xCoord=450.0]\n"
+                + "p14[yCoord=1230.0, xCoord=2070.0]\n"
+                + "p16[yCoord=330.0, xCoord=450.0]\n"
+                + "p17[yCoord=750.0, xCoord=2070.0]\n"
+                + "p18[yCoord=329.65, xCoord=2072.3]\n"
+                + "p5[yCoord=1350.0, xCoord=1290.0]\n"
+                + "p6[yCoord=704.92, xCoord=1590.0]\n"
+                + "p7[yCoord=1110.0, xCoord=1470.0]\n"
+                + "p8[yCoord=750.0, xCoord=1890.0]\n"
+                + "p9[yCoord=450.0, xCoord=1530.0]\n"
+                + "\n"
+                + ".transitions\n"
+                + "ingress[label=\"ingress\", yCoord=510.0, xCoord=570.0, tfl=\"S -> {S},> -> {S}\"]\n"
+                + "t0[label=\"t0\", yCoord=630.0, xCoord=990.0, tfl=\"S -> {C},C -> {C}\", weakFair=\"true\"]\n"
+                + "t1[label=\"t1\", yCoord=726.0, xCoord=685.0, tfl=\"S -> {A},A -> {A}\", weakFair=\"true\"]\n"
+                + "t10[label=\"t10\", yCoord=1110.0, xCoord=2070.0, weakFair=\"true\"]\n"
+                + "t11[label=\"t11\", yCoord=450.0, xCoord=2070.0, weakFair=\"true\"]\n"
+                + "t2[label=\"t2\", yCoord=870.0, xCoord=990.0, tfl=\"A -> {C},C -> {C}\", weakFair=\"true\"]\n"
+                + "t3[label=\"t3\", yCoord=750.0, xCoord=1769.0, tfl=\"P -> {P},L -> {P}\", weakFair=\"true\"]\n"
+                + "t4[label=\"t4\", yCoord=630.0, xCoord=1530.0, tfl=\"P -> {P},C -> {P}\", weakFair=\"true\"]\n"
+                + "t5[label=\"t5\", yCoord=810.0, xCoord=1590.0, tfl=\"C -> {C},L -> {C}\", weakFair=\"true\"]\n"
+                + "t6[label=\"t6\", yCoord=990.0, xCoord=1470.0, tfl=\"C -> {L},L -> {L}\", weakFair=\"true\"]\n"
+                + "t7[label=\"t7\", yCoord=1230.0, xCoord=1290.0]\n"
+                + "t9[label=\"t9\", yCoord=450.0, xCoord=450.0, weakFair=\"true\"]\n"
+                + "\n"
+                + ".flows\n"
+                + "ingress: {1*S} -> {1*S}\n"
+                + "t0: {1*p11, 1*S, 1*C} -> {1*p11, 1*C, 1*S}\n"
+                + "t1: {1*p12, 1*A, 1*S} -> {1*A, 1*S, 1*p12}\n"
+                + "t10: {1*p14, 1*p6} -> {1*p17, 1*p7}\n"
+                + "t11: {1*p9, 1*p17} -> {1*p18, 1*p8}\n"
+                + "t2: {1*p10, 1*A, 1*C} -> {1*C, 1*p10, 1*A}\n"
+                + "t3: {1*P, 1*L, 1*p8} -> {1*p8, 1*L, 1*P}\n"
+                + "t4: {1*p9, 1*C, 1*P} -> {1*p9, 1*P, 1*C}\n"
+                + "t5: {1*p6, 1*L, 1*C} -> {1*L, 1*p6, 1*C}\n"
+                + "t6: {1*C, 1*p7, 1*L} -> {1*C, 1*p7, 1*L}\n"
+                + "t7: {1*p5} -> {1*p13, 1*p14}\n"
+                + "t9: {1*p13, 1*p12} -> {1*p16, 1*p11}\n"
+                + "\n"
+                + ".initial_marking {1*A, 1*C, 1*L, 1*P, 1*S, 1*p10, 1*p12, 1*p5, 1*p6, 1*p9}";
+        PetriNetWithTransits net = PNWTTools.getPetriNetWithTransits(content, false);
         createLTLFormula(net, "A F P");
     }
 
